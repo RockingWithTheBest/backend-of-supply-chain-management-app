77373a9d93a250880b0f67afc5701ef51b9c6c41
RockingWithTheBest <sikanweclyde@gmail.com>
2025-04-24 21:53:43 +0300

    feat : DDT for Customers

---
+++ SupplyChain.Tests/Data Driven Tests/ProductTests.cs
@@ -0,0 +1,49 @@
+﻿using Back_End.Models;
+using Dapper;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupplyChain.Tests.Data_Driven_Tests
+{
+    public class ProductTests
+    {
+        private readonly string connectionString = "Server=ROCKINWITHEBEST;Database=Supply Chain Management;Integrated Security=True;Persist Security Info=False;TrustServerCertificate=True;";
+        public IEnumerable<object[]> GetLoginTestData()
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                var testData = connection.Query("SELECT Id, Name, Brand, StockQuantity, SupplierId FROM Products");
+                return testData.Select(user => new object[] { (int)user.Id, (string)user.Name, user.Brand, (int)user.StockQuantity, (int)user.SupplierId });
+            }
+        }
+        [Theory]
+        [MemberData(nameof(LoginTestData))]
+        public void TestLogin(int Id, string Name, string Brand, int StockQuantity, int SupplierId)
+        {
+            ProductEntity product = new ProductEntity();
+            product.Name = Name;
+            product.Brand = Brand;
+            product.Id = Id;
+            product.StockQuantity = StockQuantity;
+            product.SupplierId = SupplierId;
+
+
+            product.Should().Satisfy<ProductEntity>(pr =>
+            {
+                pr.Name.Should().NotBeEmpty();
+                pr.Brand.Should().NotBeEmpty();
+                pr.Id.Should().BePositive();
+                pr.StockQuantity.Should().BePositive();
+                pr.SupplierId.Should().BePositive();
+            });
+        }
+
+        public static IEnumerable<object[]> LoginTestData => new ProductTests().GetLoginTestData();
+    }
+}
--- SupplyChain.Tests/obj/Debug/net8.0/SupplyChain.Tests.AssemblyInfo.cs
+++ SupplyChain.Tests/obj/Debug/net8.0/SupplyChain.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SupplyChain.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57cf073e6f627f55a110c478451bca2d5f1a1a2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f143549dee7491cb148c84beda1f5c349e3a791")]
 [assembly: System.Reflection.AssemblyProductAttribute("SupplyChain.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SupplyChain.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
