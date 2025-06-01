76f22535017c5090ab1ccbea12d3b6bd347027c3
RockingWithTheBest <151398987+RockingWithTheBest@users.noreply.github.com>
2025-06-01 17:03:18 +0300

    feat : Remove the Inventory instance from the Method and added it within the method.

--- xx1/Repository/Implementation/InventoryRepository.cs
+++ xx1/Repository/Implementation/InventoryRepository.cs
@@ -1,84 +1,84 @@
-﻿using Back_End.Models;
-using Microsoft.EntityFrameworkCore;
-using xx1.Db;
-using xx1.Entities_Models_;
-using xx1.Repository.Interface;
+    using Back_End.Models;
+    using Microsoft.EntityFrameworkCore;
+    using xx1.Db;
+    using xx1.Entities_Models_;
+    using xx1.Repository.Interface;
 
-namespace xx1.Repository.Implementation
-{
-    public class InventoryRepository : IInventory
+    namespace xx1.Repository.Implementation
     {
-        private readonly ApplicationDBContext dBContext;
-        public InventoryRepository(ApplicationDBContext dBContext)
+        public class InventoryRepository : IInventory
         {
-            this.dBContext = dBContext;
-        }
-        public int AddInventory(InventoryEntity inventory, int OrderId)
-        {
-            var order = dBContext.Orders.Find(OrderId);
-            if (order == null)
+            private readonly ApplicationDBContext dBContext;
+            public InventoryRepository(ApplicationDBContext dBContext)
             {
-                return -1;
+                this.dBContext = dBContext;
             }
-            var customer = dBContext.Customers.Find(order.CustomerId);
-            if (customer == null)
+            public int AddInventory(int OrderId)
             {
-                return -1;
+                var order = dBContext.Orders.Find(OrderId);
+                if (order == null)
+                {
+                    return -1;
+                }
+                var customer = dBContext.Customers.Find(order.CustomerId);
+                if (customer == null)
+                {
+                    return -1;
+                }
+                var ordercheck = dBContext.OrderChecks.Where(x => x.OrderId == OrderId).FirstOrDefault();
+                var product = dBContext.Products.Where(x => x.Id == order.ProductId).FirstOrDefault();
+            InventoryEntity inventory = new InventoryEntity
+                {
+                    OrderId = order.Id,
+                    CustomerNames = customer.FirstName + " " + customer.LastName,
+                    Quantity = order.Quantity,
+                    ProductName = ordercheck.ProductName,
+                    OrderCheckId = ordercheck.Id,
+                    PricePaid = (product.Price * order.Quantity),
+                    Status = "New Order",
+                    CustomerId = order.CustomerId
+                };
+                dBContext.Inventories.Add(inventory);
+                dBContext.SaveChanges();
+                return inventory.Id;
             }
-            var ordercheck = dBContext.OrderChecks.Where(x => x.OrderId == OrderId).FirstOrDefault();
-            var product = dBContext.Products.Where(x => x.Id == order.ProductId).FirstOrDefault();
-            inventory = new InventoryEntity
+
+            public void Dispose()
             {
-                OrderId = order.Id,
-                CustomerNames = customer.FirstName + " " + customer.LastName,
-                Quantity = order.Quantity,
-                ProductName = ordercheck.ProductName,
-                OrderCheckId = ordercheck.Id,
-                PricePaid = (product.Price * order.Quantity),
-                Status = "New Order",
-                CustomerId = order.CustomerId
-            };
-            dBContext.Inventories.Add(inventory);
-            dBContext.SaveChanges();
-            return inventory.Id;
-        }
-
-        public void Dispose()
-        {
-            this.dBContext?.Dispose();
-        }
-
-        public IEnumerable<InventoryEntity> GetAllInventory()
-        {
-            var inventory = dBContext.Inventories.ToList();
-            return inventory;
-        }
-
-        public IEnumerable<InventoryEntity> GetConfirmedOrder(int customerId)
-        {
-            var inventList = dBContext.Inventories.Where(x => x.CustomerId == customerId).ToList();
-            var NewOrders = inventList.Where(x => x.Status == "Recieved Order");
-            return NewOrders;
-        }
-
-        public IEnumerable<InventoryEntity> GetNewOrder(int customerId)
-        {
-            var inventList = dBContext.Inventories.Where(x => x.CustomerId == customerId).ToList();
-            var NewOrders = inventList.Where(x => x.Status == "New Order").ToList();
-            foreach (var order in NewOrders)
-            {
-                Console.WriteLine(order.OrderId);
+                this.dBContext?.Dispose();
             }
-            return NewOrders;
-        }
 
-        public InventoryEntity UpdateOrderStatus(int Id)
-        {
-            var inventoryList = new InventoryEntity();
-            inventoryList = dBContext.Inventories.Where(x => x.Id == Id).FirstOrDefault();
-            inventoryList.Status = "Recieved Order";
-            dBContext.SaveChanges();
-            return inventoryList;
+            public IEnumerable<InventoryEntity> GetAllInventory()
+            {
+                var inventory = dBContext.Inventories.ToList();
+                return inventory;
+            }
+
+            public IEnumerable<InventoryEntity> GetConfirmedOrder(int customerId)
+            {
+                var inventList = dBContext.Inventories.Where(x => x.CustomerId == customerId).ToList();
+                var NewOrders = inventList.Where(x => x.Status == "Recieved Order");
+                return NewOrders;
+            }
+
+            public IEnumerable<InventoryEntity> GetNewOrder(int customerId)
+            {
+                var inventList = dBContext.Inventories.Where(x => x.CustomerId == customerId).ToList();
+                var NewOrders = inventList.Where(x => x.Status == "New Order").ToList();
+                foreach (var order in NewOrders)
+                {
+                    Console.WriteLine(order.OrderId);
+                }
+                return NewOrders;
+            }
+
+            public InventoryEntity UpdateOrderStatus(int Id)
+            {
+                var inventoryList = new InventoryEntity();
+                inventoryList = dBContext.Inventories.Where(x => x.Id == Id).FirstOrDefault();
+                inventoryList.Status = "Recieved Order";
+                dBContext.SaveChanges();
+                return inventoryList;
+            }
         }
     }
-}
